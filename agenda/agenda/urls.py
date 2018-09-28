e7dd4971cb882846f1164f406c170d21d39c6480
anele <anele_13@hotmail.com>
2018-09-28 19:22:13 -0300

    segundo commit

--- agenda/agenda/urls.py
+++ agenda/agenda/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from . import views
 
 urlpatterns = [
+    url(r'^$', views.home, name='home'),
+    #url(r'^login$', views.login_view, name='login'),
+    #url(r'^logout$', views.logout_view, name='logout'),
     url(r'^admin/', admin.site.urls),
 ]
